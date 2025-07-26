24792762c29dd47696c50d4c3c7bd8c255fe9238
Michael Nisi <michael.nisi@gmail.com>
2025-07-26 18:30:04 +0200

    GH-2 Add swift-system package dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-markdown", from: "0.4.0")
+        .package(url: "https://github.com/apple/swift-markdown", from: "0.4.0"),
+        .package(url: "https://github.com/apple/swift-system.git", from: "1.0.0")
     ],
     targets: [
         .executableTarget(name: "App",
@@ -23,7 +24,8 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Hummingbird", package: "hummingbird"),
                 .product(name: "Mustache", package: "swift-mustache"),
-                .product(name: "Markdown", package: "swift-markdown")
+                .product(name: "Markdown", package: "swift-markdown"),
+                .product(name: "SystemPackage", package: "swift-system") 
             ],
             resources: [
                 .process("Resources"),
