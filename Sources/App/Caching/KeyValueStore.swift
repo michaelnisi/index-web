80e32dc2506f042bc1c39872afe803aba5e2016e
Michael Nisi <michael.nisi@gmail.com>
2026-01-21 09:32:08 +0100

    GH-72 Review key-value store (#73)
    * GH-72 Make key value store safer
    
    * GH-72 Remove new post

--- Sources/App/Caching/KeyValueStore.swift
+++ Sources/App/Caching/KeyValueStore.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-actor KeyValueStore<Key: Hashable, Value> {
+actor KeyValueStore<Key: Hashable & Sendable, Value: Sendable> {
     private var storage: [Key: Value] = [:]
 
     init(initialValues: [Key: Value] = [:]) {
@@ -11,11 +11,32 @@ actor KeyValueStore<Key: Hashable, Value> {
         storage[key]
     }
 
-    func set(_ key: Key, value: Value) {
-        storage[key] = value
+    @discardableResult
+    func updateValue(_ value: Value, forKey key: Key) -> Value? {
+        storage.updateValue(value, forKey: key)
     }
 
-    func remove(_ key: Key) {
+    @discardableResult
+    func modify<R>(_ key: Key, _ body: @Sendable (inout Value?) throws -> R) rethrows -> R {
+        var value = storage[key]
+        let result = try body(&value)
+        storage[key] = value
+        return result
+    }
+
+    @discardableResult
+    func updateValue(forKey key: Key, default defaultValue: @autoclosure () -> Value, _ transform: @Sendable (inout Value) -> Void) -> Value {
+        if storage[key] == nil {
+            storage[key] = defaultValue()
+        }
+        var current = storage[key]!
+        transform(&current)
+        storage[key] = current
+        return current
+    }
+
+    @discardableResult
+    func removeValue(forKey key: Key) -> Value? {
         storage.removeValue(forKey: key)
     }
 
@@ -25,6 +46,18 @@ actor KeyValueStore<Key: Hashable, Value> {
 
     var count: Int {
         storage.count
+    }
+
+    func contains(_ key: Key) -> Bool {
+        storage[key] != nil
+    }
+
+    func values() -> [Value] {
+        Array(storage.values)
+    }
+
+    func allItems() -> [(Key, Value)] {
+        Array(storage)
     }
 
     func keys() -> [Key] {
